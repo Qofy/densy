d65fa24f90d9cacc8812962f9911111337d85ff9
Qofy <148817944+Qofy@users.noreply.github.com>
2025-05-28 06:32:59 +0200

    Creating real app - clone of Densy

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+// import styled from "styled-components"
+import "../styles/login.scss"
+
+export const Login =(props) => {
+  return(
+    <section className=" login-section">
+    <div className="content">
+    Login
+    </div>
+    </section>
+  )
+}
