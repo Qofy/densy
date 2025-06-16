4735337a21815b5701432dc9ca2ada5328ad913e
Qofy <148817944+Qofy@users.noreply.github.com>
2025-06-16 13:28:17 +0200

    added the logout button. this function works when you hover on the user-login photo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,11 @@ import "../styles/header.scss";
 import { headerNav } from "../data/header.ts";
 import { useDispatch, useSelector } from "react-redux"; 
 import { useNavigate } from "react-router-dom";
-import { selectUserEmail, selectUserName, selectUserPhoto, setUserLoginDetails } from "../features/userSlice.js";
+import { selectUserEmail, selectUserName, selectUserPhoto, setSignOutState, setUserLoginDetails } from "../features/userSlice.js";
 import { auth, provider } from "../firebase.js";
 import { signInWithPopup, onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
+import { RiDropdownList } from "react-icons/ri";
 
 export default function Header() {
   const dispatch = useDispatch();
@@ -27,15 +28,36 @@ export default function Header() {
   }, [dispatch, navigate]);
 
   const handleAuth = () => {
+    if(!username){
     signInWithPopup(auth, provider)
       .then((result: any) => {
         setUser(result.user);
-        navigate("/");//later change it to home
+        navigate("/home");//later change it to home
       })
       .catch((error: any) => {
         alert(error.message);
       });
+      }
+      else if(username){
+        auth.signOut().then(()=> {
+      dispatch(setSignOutState());
+      navigate("/")
+    })
+    .catch((error:any) => {
+      alert(error.message)
+    })
+      }
   }
+
+  // const handleSignOut = ()=>{
+  //   auth.signOut().then(()=> {
+  //     dispatch(setSignOutState());
+  //     navigate("/")
+  //   })
+  //   .catch((error:any) => {
+  //     alert(error.message)
+  //   })
+  // }
 
   const setUser = (user: any) => {
     dispatch(
@@ -72,7 +94,14 @@ export default function Header() {
               );
             })}
           </nav>
+          <div className="drop-down">
           <img className="user-photo" src={userPhoto} alt={username} />
+            <div className="dropdown-menu">
+              <button className="dropdown-item" onClick={handleAuth}>
+                Sign out
+              </button>
+            </div>
+          </div>
         </>
       )}
     </header>
