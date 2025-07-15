e02adc53dfd5e13c750c43bfeadbd3f7414264bd
Qofy <148817944+Qofy@users.noreply.github.com>
2025-07-15 06:17:54 +0200

    Added trailer and ply button to the details page

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,5 +1,6 @@
 import "../styles/details.scss";
 
+import {Play} from "lucide-react";
 function Details(){
   return(
     <div className="details-container">
@@ -9,6 +10,18 @@ function Details(){
 
       <div className="image-title">
         <img src="https://image.tmdb.org/t/p/original/h40hblm8J1if7T2CBMjCD85HwuD.png" alt="" />
+      </div>
+      <div className="contentMeta">
+        <div className="controls">
+          <div className="player">
+            <Play  color="black" size={30} />
+            <span>Play</span>
+          </div>
+          <div className="trailer">
+            <Play color="white" size={30} />
+            <span>Trailer</span>
+          </div>
+        </div>
       </div>
     </div>
   )
